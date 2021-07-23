2ca84d75eff29042d3c32af8f58807846200e978
Athranesh <athranesh@gmail.com>
2021-07-23 14:33:29 +0300

    add unpkg path plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -18,12 +19,20 @@ const App = () => {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
-    });
+    const result = await ref.current.build(
+      {
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin()],
+      },
+      {
+        loader: 'jsx',
+        target: 'es2015',
+      }
+    );
 
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   useEffect(() => {
