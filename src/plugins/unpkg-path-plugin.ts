afb59387c05790d720400f79ad28287371a9dccb
Athranesh <athranesh@gmail.com>
2021-08-13 15:06:18 +0300

    move onLoad to a separate fetch plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,25 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
 
-const fileCache = localForage.createInstance({
-  name: 'filecache',
-});
-
-/*
-  onResolve -> path
-  onLoad -> loading the file
-  filter -> regex used to define which files triggers which onResolve/onLoad
-  namespace -> Must be set if path is not an absolute file path. Similar to filter, defined for files returned by onResolve, can be defined in the object that is the first argument of onLoad, if namespaces do not match, onLoad wont run. Example:
-
-  The following onLoad won't run for the files returned by onResolve
-  onResolve=()=>{namespace:a}
-  onLoad=()=>{namespace:b}
-
-  onResolve and onLoad run after each other repeatedly until all files (such as imports) are loaded.
-*/
-
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -43,35 +24,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         };
-      });
-
-      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-        //caching
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
-        };
-
-        await fileCache.setItem(args.path, result);
-
-        return result;
       });
     },
   };
