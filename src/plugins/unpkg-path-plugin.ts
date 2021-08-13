391eff883fa8419ec9dca6ac1b5bd94230e1af2a
Athranesh <athranesh@gmail.com>
2021-08-13 13:20:31 +0300

    finish unpkg plugin, install localforage

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,31 +1,61 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+
+/*
+  onResolve -> path
+  onLoad -> loading the file
+  filter -> regex used to define which files triggers which onResolve/onLoad
+  namespace -> Must be set if path is not an absolute file path. Similar to filter, defined for files returned by onResolve, can be defined in the object that is the first argument of onLoad, if namespaces do not match, onLoad wont run. Example:
+
+  The following onLoad won't run for the files returned by onResolve
+  onResolve=()=>{namespace:a}
+  onLoad=()=>{namespace:b}
+
+  onResolve and onLoad run after each other repeatedly until all files (such as imports) are loaded.
+*/
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+        //Handling relative files
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
+        }
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              const message = require('react');
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data, request } = await axios.get(args.path);
+
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
       });
     },
   };
